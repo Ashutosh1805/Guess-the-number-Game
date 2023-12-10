fde1afe849b94856176f043c2a0fcd4e154fe497
Ashutosh1805 <150644515+Ashutosh1805@users.noreply.github.com>
2023-12-10 13:29:45 +0530

    Add files via upload

---
+++ Game to guess numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>//the lib which includes the code to print random numbers
+int main()
+{
+	int number,guess,nguesses;
+	srand(time(0));//code to print random numbers
+	number=rand()%100+1;
+	//printf("the number is %d\n",number);
+	do{
+		printf("Guess a number between 1 to 100/n");
+		scanf("%d",&guess);
+		if(guess>number)
+		{
+			printf("Lower case please");
+		}
+		else if(guess<number){
+			printf("Uppercase please");
+		}
+		else{
+			printf("You guessed it right in %d attempts\n",nguesses);
+		}
+		nguesses++;
+	}while(guess!=number);
+	return 0;
+}
